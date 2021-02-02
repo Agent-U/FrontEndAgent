8852f15cbc6024ea71ddd069f47e68753285a9b6
Brahimi Youcef <youcef.brahimi.etu@univ-lille.fr>
2021-02-02 15:07:43 +0100

    FEAT: ajout du tableau de gestion des etudiants.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { CourrierComponent } from './components/courrier/courrier.component';
+import { EtudiantsComponent } from './components/etudiants/etudiants.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:AccueilComponent},
+  {path:"etudiants",component:EtudiantsComponent},
+  {path:"courriers",component:CourrierComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
---
+++ src/app/models/etudiant.ts
@@ -0,0 +1,13 @@
+export interface Etudiant {
+        ine: string;
+        nom: string;
+        prenom: string;
+        mail: string;
+        motDePasse: string;
+        petiteEnveloppe: number;
+        grandeEnveloppe: number;
+        avisPassage: number;
+        colis: number;
+        chambre: string;
+}
+
